0d3100cb2e7de748cc6fbfe048d05220bf2bdea4
Jules Goguely <j_goguely@Juless-MacBook-Pro.local>
2014-02-08 17:10:53 -0500

    forum added

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -4,4 +4,20 @@ class PicsController < ApplicationController
 		@pics = Pic.all 
 	end
 
+	def new
+		@pic = Pic.new 
+	end
+
+	def create
+		Pic.create( pics_params )
+		redirect_to pics_path
+	end
+
+	private
+	def pics_params
+		params.require(:pic).permit(
+			:hour, :emotion, :learned
+			)
+	end
+
 end
